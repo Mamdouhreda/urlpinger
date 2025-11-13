9221d0c0d839e7e6ab82a0a636923531ec41fb64
mamdohreda <mamdohreda@gmail.com>
2025-11-13 15:31:11 +0000

    just before move main branch

--- main.go
+++ main.go
@@ -2,24 +2,29 @@ package main
 
 import (
 	"fmt"
-
+	"sync"
 	"urlpinger/data"
 	loadevent "urlpinger/load"
 )
 
 func main() {
+	var wg sync.WaitGroup
     for _, site := range data.Sites {
+        wg.Add(1)
+        go func(url string) {
+            defer wg.Done()
         navTiming, err := loadevent.LoadEventMS(site.URL)
         if err != nil {
-            fmt.Printf("Error fetching metrics for %s: %v\n", site.URL, err)
-            continue
+            fmt.Printf("Error fetching metrics for %s: %v\n", url, err)
+            return
         }
-
         fmt.Println("URL:", site.URL)
         fmt.Println("LoadEvent:", navTiming.LoadEvent, "seconds")
         fmt.Println("TTFB:", navTiming.TTFB, "milliseconds")
         fmt.Println("DNS:", navTiming.DNS, "milliseconds")
         fmt.Println("TLS:", navTiming.TLS, "milliseconds")
-        fmt.Println()
-    }
+        fmt.Println("ID:", site.ID)
+    }(site.URL)
+}
+    wg.Wait()
 }

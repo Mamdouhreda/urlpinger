daf6a123fd41e089662cd23c23ec0391db7ec5dc
mamdohreda <mamdohreda@gmail.com>
2025-11-14 22:51:54 +0000

    add channel

--- main.go
+++ main.go
@@ -2,26 +2,26 @@ package main
 
 import (
 	"fmt"
-	//"sync"
+	"sync"
 	"time"
 	"urlpinger/data"
 	loadevent "urlpinger/load"
 )
 
+const MaxWorkers = 5 // Maximum number of concurrent workers
 func main() {
     start := time.Now()
-	//var wg sync.WaitGroup
-
+	var wg sync.WaitGroup
+    sem := make(chan struct{}, MaxWorkers) // create the channel with the limit of the workers
     for _, site := range data.Sites {
-       // wg.Add(1)
-
-        // capture values
+       wg.Add(1)
+       sem <- struct{}{} // sending the token 
         url := site.URL
         id := site.ID
 
-        func(url string, id int) {
-            //defer wg.Done()
-
+        go func(url string, id int) {
+            defer wg.Done()
+            defer func() { <-sem }() // Release the token 
             navTiming, err := loadevent.LoadEventMS(url)
             if err != nil {
                 fmt.Printf("Error fetching metrics for %s: %v\n", url, err)
@@ -37,7 +37,7 @@ func main() {
             fmt.Println()
         }(url, id)
     }
-   // wg.Wait()
+    wg.Wait()
     end := time.Now()
     fmt.Println("==== FINISHED ====")
     fmt.Printf("Time: %.2f seconds\n", end.Sub(start).Seconds())
